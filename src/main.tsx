16679068b1fef1ef4a24a76341b25d2c90709ed0
Ashutosh Nayak <132771894+nayakashutosh@users.noreply.github.com>
2026-04-15 13:51:06 +0530

    Fix import statement for createRoot in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+// @ts-ignore
 import App from './App.jsx'
 import './index.css'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
